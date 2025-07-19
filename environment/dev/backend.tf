13acba085ca484f5b83135278861bffea74e278d
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-07-19 21:14:50 +0530

    backend storage account name - removed

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "backend-rg"
-    storage_account_name = "backendstg-rakesh"
+    storage_account_name = "backendstgrakesh"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"  
     
