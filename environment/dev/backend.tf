ead6d9c74f1961108cbcddd39a528f8736f6dea1
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-07-22 15:48:29 +0530

    removed shell

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "backend-rg"
     storage_account_name = "backendstgrakesh"
     container_name       = "tfstatefile"
-    key                  = "terraform.tfstate"  
-    
+    key                  = "terraform.tfstate"
+
   }
 }
--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,10 +1,10 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  
+
 }
 
 variable "resource_group_location" {
-  
+
 }
 
